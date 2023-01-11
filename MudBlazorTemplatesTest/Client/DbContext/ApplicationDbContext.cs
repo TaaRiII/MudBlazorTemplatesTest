f74dbab899da9534a63b9c0ac434020da81220ba
Your Name <you@example.com>
2023-01-12 02:41:16 +0500

    Add project files.

---
+++ MudBlazorTemplatesTest/Client/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿//using static MudBlazor.Icons;
+//using System.Collections.Generic;
+
+//namespace MudBlazorTemplatesTest.Client.DbContext
+//{
+//    public class ApplicationDbContext : DbContext
+//    {
+//        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+//        {
+//        }
+//        public DbSet<Customer> Customers { get; set; }
+//    }
+//}
